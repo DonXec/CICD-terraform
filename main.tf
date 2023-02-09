35cb482e61e274a49687dceda9d1890cc3740c18
Abdurrahman Yildirim <abdurrahman.yildirim@student.techstarter.de>
2023-02-09 22:47:30 +0100

    change port

--- main.tf
+++ main.tf
@@ -118,7 +118,7 @@ resource "azurerm_network_security_rule" "port8080" {
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
-  destination_port_range      = "22"
+  destination_port_range      = "8080"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = local.Ressource_Group_Name
